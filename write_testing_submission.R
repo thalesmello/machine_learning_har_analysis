46d12da40b06ec29543b627843362d87780a490a
Thales Mello <thalesmello@gmail.com>
2014-11-23 18:33:33 -0200

    included function to save files for each problem

--- write_testing_submission.R
+++ write_testing_submission.R
@@ -5,5 +5,14 @@ submission <- subset(submission, select = -c(X, user_name, raw_timestamp_part_1,
 knnSubmission <- predict(knnObject, newdata = submission)
 pcaSubmission <- predict(pcaObject, newdata = knnSubmission)
 submissionPrediction <- predict(model, newdata = pcaSubmission)
-submissionPrediction
+submissionPrediction <- as.character(submissionPrediction)
 
+pml_write_files = function(x){
+  n = length(x)
+  for(i in 1:n){
+    filename = paste0("problem_id_",i,".txt")
+    write.table(x[i],file=filename,quote=FALSE,row.names=FALSE,col.names=FALSE)
+  }
+}
+
+pml_write_files(submissionPrediction)
